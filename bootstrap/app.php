d2e2ba1917ca123f4bc58c95d328affbe5ff300e
risyady <ibrahimrisyad28@gmail.com>
2026-04-25 17:35:27 +0700

    fix CORS config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,10 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
+
+        $middleware->use([
+            \Illuminate\Http\Middleware\HandleCors::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(function (\Illuminate\Validation\ValidationException $e, $request) {
--- config/cors.php
+++ config/cors.php
@@ -1,10 +1,23 @@
+<?php
+
 return [
-    'paths' => ['api/*'],
+    'paths' => ['api/*', '/'],
+
     'allowed_methods' => ['*'],
+
     'allowed_origins' => [
         'http://localhost:3000',
-        'https://sintoga-learn.vercel.app',
+        'http://localhost:8000',
+        'https://sintoga-learn.vercel.app'
     ],
+
+    'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
     'supports_credentials' => true,
 ];
