dbfb0778a12a8dd9b4fa910a2a6dcf93fee99fec
risyady <ibrahimrisyad28@gmail.com>
2026-05-07 20:07:40 +0700

    test s3

--- routes/api.php
+++ routes/api.php
@@ -55,3 +55,18 @@ Route::middleware('auth:sanctum')->group(function() {
         Route::put('ujian/{siswaUjian}/isian', [NilaiController::class, 'inputFillInBlank']);
     });
 });
+
+Route::get('test-s3', function () {
+    try {
+        Storage::disk('s3')->put('test.txt', 'Hello Tigris!', 'public'); // 👈 tambah public
+        $url = Storage::disk('s3')->url('test.txt');
+        return response()->json([
+            'message' => 'Berhasil!',
+            'url'     => $url,
+        ]);
+    } catch (\Exception $e) {
+        return response()->json([
+            'message' => 'Gagal: ' . $e->getMessage(),
+        ], 500);
+    }
+});
