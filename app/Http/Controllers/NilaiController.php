fabb45017db87b848598b6dd98795bee97057c73
risyady <ibrahimrisyad28@gmail.com>
2026-05-16 15:24:51 +0700

    minor fix variable in listManual

--- app/Http/Controllers/NilaiController.php
+++ app/Http/Controllers/NilaiController.php
@@ -31,7 +31,7 @@ class NilaiController extends Controller
         $siswaUjians = SiswaUjian::where('ujian_id', $ujian->id)
             ->where('status', 'dikirim')
             ->with([
-                'siswa:id,name,email',
+                'siswa:id,nama,email',
                 'jawaban' => function ($query) {
                     $query->whereHas('soal', fn($q) =>
                         $q->whereIn('tipe_soal', ['isian', 'essay'])
